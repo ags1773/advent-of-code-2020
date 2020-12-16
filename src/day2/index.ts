4cf6ed55bf41791b612aed037ee07d5e9dcd9d83
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-12-17 00:18:42 +0530

    day2part2

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -9,6 +9,24 @@ export const day2 = (data: DayTwoPropTypes[]): number => {
   return validPasswords;
 };
 
+export const day2Part2 = (data: DayTwoPropTypes[]): number => {
+  let validPasswords = 0;
+  data.forEach((entry: DayTwoPropTypes) => {
+    const { lowerLimit, higherLimit, alphabet, password } = entry;
+    const alphabet1 = password[lowerLimit - 1];
+    const alphabet2 = password[higherLimit - 1];
+    if (
+      (alphabet1 === alphabet && alphabet2 !== alphabet) ||
+      (alphabet1 !== alphabet && alphabet2 === alphabet)
+    ) {
+      validPasswords++;
+    }
+  });
+
+  console.log(`Number of valid passwords >> ${validPasswords}`);
+  return validPasswords;
+};
+
 interface DayTwoPropTypes {
   lowerLimit: number;
   higherLimit: number;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { runDay1, runDay2 } from "./helpers";
 import { day1, dayOnePartTwo } from "./day1";
-import { day2 } from "./day2";
+import { day2, day2Part2 } from "./day2";
 import path from "path";
 
 // runDay1(path.join(__dirname, "../data/day1/input"), day1);
 // runDay1(path.join(__dirname, "../data/day1/input"), dayOnePartTwo);
 
-runDay2(path.join(__dirname, "../data/day2/input"), day2);
+// runDay2(path.join(__dirname, "../data/day2/input"), day2);
+runDay2(path.join(__dirname, "../data/day2/input"), day2Part2);
