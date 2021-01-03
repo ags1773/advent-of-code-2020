a04f0bdafa797099f4136140f69a61d88b405ed0
Amogh Sahasrabhojanee <amogh@quintype.com>
2021-01-03 21:32:14 +0530

    day3Part2

--- src/day3/index.ts
+++ src/day3/index.ts
@@ -1,12 +1,35 @@
-// right 3, down 1. How many trees would you encounter?
-
 export const day3 = (data: string[]): number => {
+  return checkTrees({
+    data,
+    down: 1,
+    right: 3,
+  });
+};
+
+export const day3Part2 = (data: string[]): number => {
+  const r1d1 = checkTrees({ data, right: 1, down: 1 });
+  const r3d1 = checkTrees({ data, right: 3, down: 1 });
+  const r5d1 = checkTrees({ data, right: 5, down: 1 });
+  const r7d1 = checkTrees({ data, right: 7, down: 1 });
+  const r1d2 = checkTrees({ data, right: 1, down: 2 });
+
+  return r1d1 * r3d1 * r5d1 * r7d1 * r1d2;
+};
+
+const checkTrees = ({ data, down, right }: CheckTreesProps): number => {
   let numberOfTreesEncountered = 0;
   let ptr = 0;
-  for (const row of data) {
+  for (const [idx, row] of data.entries() as any) {
+    if (idx % down !== 0) continue;
     if (row[ptr] === "#") numberOfTreesEncountered++;
-    ptr = ptr + 3;
+    ptr = ptr + right;
     ptr = ptr % row.length;
   }
   return numberOfTreesEncountered;
 };
+
+interface CheckTreesProps {
+  data: string[];
+  down: number;
+  right: number;
+}
--- src/day3/tests.js
+++ src/day3/tests.js
@@ -4,7 +4,7 @@ const helpers = require("../../dist/helpers");
 const main = require("../../dist/day3");
 
 const { runDay3 } = helpers;
-const { day3 } = main;
+const { day3, day3Part2 } = main;
 
 describe("Day 3", () => {
   it("should work with test input", async () => {
@@ -20,5 +20,22 @@ describe("Day 3", () => {
       day3
     );
     assert.strictEqual(output, 292);
+  });
+});
+
+describe("Day 3 Part 2", () => {
+  it("should work with test input", async () => {
+    const output = await runDay3(
+      path.join(__dirname, "../../data/day3/test-input"),
+      day3Part2
+    );
+    assert.strictEqual(output, 336);
+  });
+  it("should work with input", async () => {
+    const output = await runDay3(
+      path.join(__dirname, "../../data/day3/input"),
+      day3Part2
+    );
+    assert.strictEqual(output, 9354744432);
   });
 });
